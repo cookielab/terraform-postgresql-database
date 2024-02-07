109a79fbd30269db83320e94f40560988ac26858
Jakub Jindra <jakub.jindra@cookielab.io>
2024-02-07 10:07:56 +0100

    updated example to match the changes

--- examples/main/main.tf
+++ examples/main/main.tf
@@ -41,7 +41,7 @@ provider "postgresql" {
   superuser       = false
   database        = "postgres"
   username        = "postgres"
-  password        = "password"
+  password        = ""
   sslmode         = "disable"
   connect_timeout = 15
 }
@@ -49,9 +49,7 @@ provider "postgresql" {
 module "database" {
   source = "../.."
 
-  database_private_hostname = "postgres.example.com:5432"
-
-  databases          = local.databases
-  user_role          = merge(local.databases, local.users)
-  create_access_user = true
+  databases   = local.databases
+  user_role   = merge(local.databases, local.users)
+  create_role = true
 }
